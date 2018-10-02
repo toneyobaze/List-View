2b0374b8ca05826b16f70937eef231ba1129f873
Anthony <you@example.comcls>
2018-10-02 18:12:59 +0000

    added Log code

--- app/src/main/java/com/example/oladapo/listview/MainActivity.java
+++ app/src/main/java/com/example/oladapo/listview/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.oladapo.listview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -16,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
 
         ListView myListView = findViewById(R.id.myListView);
 
-        ArrayList<String> myFamily = new ArrayList<String>();
+        final ArrayList<String> myFamily = new ArrayList<String>();
 
         myFamily.add("Alexander");
         myFamily.add("Camilla");
@@ -28,5 +31,14 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<String> arrayAdapter =new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, myFamily);
 
         myListView.setAdapter(arrayAdapter);
+
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Log.i("Person tapped:", myFamily.get(position));
+
+            }
+        });
     }
 }
