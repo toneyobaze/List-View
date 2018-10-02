1d26591c7900a1111745fa27c8f7ca7cb0df1885
Anthony <you@example.comcls>
2018-10-02 17:50:40 +0000

    Second Commit

--- app/src/main/java/com/example/oladapo/listview/MainActivity.java
+++ app/src/main/java/com/example/oladapo/listview/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.oladapo.listview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView myListView = findViewById(R.id.myListView);
+
+        ArrayList<String> myFamily = new ArrayList<String>();
+
+        myFamily.add("Alexander");
+        myFamily.add("Camilla");
+        myFamily.add("Anthony");
+        myFamily.add("Rita");
+        myFamily.add("Frances");
+        myFamily.add("Esther");
+
+        ArrayAdapter<String> arrayAdapter =new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, myFamily);
+
+        myListView.setAdapter(arrayAdapter);
     }
 }
